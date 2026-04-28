b70d73d9b2892f56bdae16f2df7697567b11fd06
elhajjame <el-hajjame@outlook.com>
2026-04-28 15:25:21 +0100

    feat(hero): complete Hero section JSX and styling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,15 @@
 import Navbar from "./Navbar";
-import Hero from "./Hero";
 import MovieList from "./MovieList";
 import MovieCard from "./MovieCard";
 import FilterBar from "./FilterBar";
 import TopMovies from "./TopMovies";
+import Hero from "./Hero";
 
 export default function Home() {
   return (
     <div className="">
       <Navbar />
+      <Hero />
     </div>
   );
 }
